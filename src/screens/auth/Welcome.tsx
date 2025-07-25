66d0138d72eae666eef582dbccff54817b040cdb
Nikhil Siwan <developer.nikk@gmail.com>
2025-07-25 18:30:22 +0530

    Welcome Page UI Completed

--- src/screens/auth/Welcome.tsx
+++ src/screens/auth/Welcome.tsx
@@ -1,6 +1,6 @@
 import { useNavigation, useTheme } from "@react-navigation/native";
-import React, { useContext, useEffect} from "react";
-import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity,StatusBar } from "react-native";
 import { useDispatch } from "react-redux";
 import { LocalizationContext } from "../../localization/localization";
 import { setAuth } from "../../redux/Reducers/userData";
@@ -10,37 +10,73 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const Welcome: React.FC = () => {
   const { colors } = useTheme();
-  const { localization } = useContext(LocalizationContext);
   const dispatch = useDispatch();
   const navigation: any = useNavigation();
-  const logo = require('../../assets/logo.png')
-  const googleLogo = require('../../assets/icons/Google.png')
-  const appleLogo = require('../../assets/icons/Apple.png')
+  const logo = require('../../assets/logo.png');
+  const googleLogo = require('../../assets/icons/Google.png');
+  const appleLogo = require('../../assets/icons/Apple.png');
+
+  
+  const [email, setEmail] = useState('');
+  
+  const [isGoogleSelected, setIsGoogleSelected] = useState(false);
+  const [isAppleSelected, setIsAppleSelected] = useState(false);
+  const isEmailInputFilled = email.length > 0;
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{flex:1}}>
+        <StatusBar
+        animated={true}
+        backgroundColor="white" 
+        barStyle="dark-content"
+        hidden={false}
+      />
         <View style={styles.parent}>
           <Image source={logo} style={styles.logo}/>
           <Text style={styles.welcomeText}> Log in or sign up</Text>
           <Text style={styles.emailText}>Email</Text>
           <TextInput
-            style={styles.input}
+            style={[
+              styles.input,
+              { borderColor: isEmailInputFilled ? '#757575' : '#DBDBDB' } 
+            ]}
             placeholder="Your email..."
             placeholderTextColor='#757575'
-          >  
+            keyboardType="email-address"
+            value={email} 
+            onChangeText={setEmail} 
+          >
           </TextInput>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              { backgroundColor: isEmailInputFilled ? '#242528' : '#DBDBDB' } 
+            ]}
+            disabled={!isEmailInputFilled} 
+          >
             <Text style={styles.buttonText}>Continue</Text>
           </TouchableOpacity>
           <Text style={styles.orText}>OR</Text>
-          <TouchableOpacity style={styles.googleButton}>
+          <TouchableOpacity
+            style={[
+              styles.googleButton,
+              { borderColor: isGoogleSelected ? '#757575' : '#DBDBDB' } 
+            ]}
+            onPress={() => setIsGoogleSelected(!isGoogleSelected)} 
+          >
             <Image source={googleLogo} style={styles.buttonlogo} />
             <Text style={{color:'#050505', fontSize:15, fontFamily:'Poppins-Medium',textAlign:'center', alignSelf:'center', lineHeight:25, right:15,}}>
               Continue with Google
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.appleButton}>
+          <TouchableOpacity
+            style={[
+              styles.appleButton,
+              { borderColor: isAppleSelected ? '#757575' : '#DBDBDB' } 
+            ]}
+            onPress={() => setIsAppleSelected(!isAppleSelected)} 
+          >
             <Image source={appleLogo} style={styles.buttonlogo}/>
             <Text style={{color:'#050505',fontSize:15, fontFamily:'Poppins-Medium', textAlign:'center', alignSelf:'center', lineHeight:25, right:15}}>
               Continue with Apple
@@ -67,11 +103,13 @@ const styles = StyleSheet.create({
   welcomeText:{
     fontSize:28,
     fontFamily:'Poppins-Regular',
+    lineHeight:35,
   },
   emailText:{
     fontFamily:'Poppins-Light',
     alignSelf:'flex-start',
     fontSize:14,
+    lineHeight:24,
     marginVertical:10,
   },
   input:{
@@ -84,7 +122,6 @@ const styles = StyleSheet.create({
     fontFamily:'Poppins-Regular',
   },
   button:{
-    backgroundColor:'#DBDBDB',
     width:'100%',
     height:55,
     borderRadius:15,
